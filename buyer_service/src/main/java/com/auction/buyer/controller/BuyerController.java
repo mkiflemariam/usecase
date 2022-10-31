c2660835aab7492dd0f07ce2b8d2790af1eb2c13
Kiflemariam <958056@cognizant.com>
2022-10-30 21:05:41 -0400

    feign updated in buyer service

--- buyer_service/src/main/java/com/auction/buyer/controller/BuyerController.java
+++ buyer_service/src/main/java/com/auction/buyer/controller/BuyerController.java
@@ -1,35 +1,62 @@
 package com.auction.buyer.controller;
 
-import com.auction.buyer.dto.BuyerRequestDto;
-import com.auction.buyer.dto.ProductResponseDto;
+
+import com.auction.buyer.request.CreateBuyerRequest;
+import com.auction.buyer.response.BuyerResponse;
+import com.auction.buyer.response.ProductResponse;
 import com.auction.buyer.service.BuyerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/e-aution/api/buyer")
 public class BuyerController {
 
+//    @Autowired
+//    private BuyerService buyerService;
+//
+//    @PostMapping("/place-bid")
+//    public ResponseEntity<?> insertBuyer(@RequestBody BuyerRequestDto buyerRequestDto ){
+//
+//        return new ResponseEntity(buyerService.creatBuyer(buyerRequestDto), HttpStatus.CREATED);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> getById(@PathVariable String id){
+//        return ResponseEntity.ok(buyerService.getBuyId(id));
+//    }
+//
+//    @PutMapping("/update-bid/{productId}/{buyerEmail}/{newBidAmount}")
+//    public ResponseEntity<?> updateBidAmount(@PathVariable String productId, @PathVariable String buyerEmail, @PathVariable BigDecimal newBidAmount ){
+//        return new ResponseEntity<ProductResponseDto>(buyerService.updateBidAmount(productId,buyerEmail,newBidAmount), HttpStatus.OK);
+//    }
+
+
     @Autowired
-    private BuyerService buyerService;
+    BuyerService buyerService;
+
 
     @PostMapping("/place-bid")
-    public ResponseEntity<?> insertBuyer(@RequestBody BuyerRequestDto buyerRequestDto ){
+    public ResponseEntity<?> createBuyer(@Valid @RequestBody CreateBuyerRequest createBuyerRequest) {
 
-        return new ResponseEntity(buyerService.creatBuyer(buyerRequestDto), HttpStatus.CREATED);
+        return new ResponseEntity<BuyerResponse>(buyerService.createBuyer(createBuyerRequest), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable String id){
-        return ResponseEntity.ok(buyerService.getBuyId(id));
+    public ResponseEntity<BuyerResponse> getById(@PathVariable String id) {
+        return ResponseEntity.ok(buyerService.getById(id));
     }
 
+
     @PutMapping("/update-bid/{productId}/{buyerEmail}/{newBidAmount}")
-    public ResponseEntity<?> updateBidAmount(@PathVariable String productId, @PathVariable String buyerEmail, @PathVariable BigDecimal newBidAmount ){
-        return new ResponseEntity<ProductResponseDto>(buyerService.updateBidAmount(productId,buyerEmail,newBidAmount), HttpStatus.OK);
+    public ResponseEntity<?> updateBidAmount(@PathVariable String productId,
+                                                           @PathVariable String buyerEmail, @PathVariable BigDecimal newBidAmount) {
+        return new ResponseEntity<ProductResponse>(buyerService.updateBidAmount(productId, buyerEmail, newBidAmount), HttpStatus.OK);
+
     }
 }
--- buyer_service/src/main/java/com/auction/buyer/domain/Buyer.java
+++ buyer_service/src/main/java/com/auction/buyer/domain/Buyer.java
@@ -1,19 +1,15 @@
 package com.auction.buyer.domain;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
-@Data
+
 @Builder
-@Setter
-@Getter
+
 public class Buyer {
 
     @Id
@@ -26,5 +22,81 @@ public class Buyer {
     private String addressId;
     private BigDecimal bidAmount;
 
+    public Buyer() {
+    }
 
+    public Buyer(String buyerId, String firstName, String lastName, String email, String phone, String productId, String addressId, BigDecimal bidAmount) {
+        this.buyerId = buyerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.productId = productId;
+        this.addressId = addressId;
+        this.bidAmount = bidAmount;
+    }
+
+    public String getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(String buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+    public BigDecimal getBidAmount() {
+        return bidAmount;
+    }
+
+    public void setBidAmount(BigDecimal bidAmount) {
+        this.bidAmount = bidAmount;
+    }
 }
